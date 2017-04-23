98e4ec2010dd3e8a79db45f77e8a9a3ed2cfa7e9
Jon Woo <jw56578@gmail.com>
2017-04-22 19:19:27 -0500

    add gh-pages

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
+import React from "react";
 
- function ProductDetail(props){
-     var {name,description,rating,price,imgUrl} = props.product;
-     var stars = [];
-     for(var i = 0; i< rating; i ++){
-         stars.push(<span className="glyphicon glyphicon-star"></span>);
-     }
-     
-     return (
-            <div className="col-sm-4 col-lg-4 col-md-4">
-              <div className="thumbnail">
-                  <img style={{width:"320px",height:"150px"}} src={imgUrl} alt=""/>
-                  <div className="caption">
-                       <h4><a href="#">{name}</a>
-                      </h4>
-                      <p>{description} 
-                      </p>
-                  </div>
-                  <div className="ratings">
-                      <p className="pull-right">15 reviews</p>
-                      <p>
-                          {stars}
-                      </p>
-                  </div>
-              </div>
-          </div>
+function ProductDetail(props) {
+  const {name,description,rating,imgUrl} = props.product;
+  const stars = [];
+  for (let i = 0; i < rating; i++) {
+    stars.push(<span className="glyphicon glyphicon-star" />);
+  }
 
-    );
+  return (
+    <div className="col-sm-4 col-lg-4 col-md-4">
+      <div className="thumbnail">
+        <img style={{width: "320px",height: "150px"}} src={imgUrl} alt="" />
+        <div className="caption">
+          <h4><a href="#">{name}</a>
+          </h4>
+          <p>{description} 
+          </p>
+        </div>
+        <div className="ratings">
+          <p className="pull-right">15 reviews</p>
+          <p>
+            {stars}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default ProductDetail;
